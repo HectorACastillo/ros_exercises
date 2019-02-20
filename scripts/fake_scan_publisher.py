7cdb97c2e44f6bdb8a8f7d43d11ba8809d5be4cc
Hector <hectorc@mit.edu>
2019-02-20 16:00:51 -0500

    commit for question 7

--- scripts/fake_scan_publisher.py
+++ scripts/fake_scan_publisher.py
@@ -5,19 +5,20 @@ from random import *
 import math
 
 def scanner():
-    pub = rospy.Publisher('fake_scan', LaserScan, queue_size=10)
-    rospy.init_node('fake_scan_publisher', anonymous=True)
-    rate = rospy.Rate(20) # 20 hz
+    pub_topic = rospy.get_param("/fake_scan_publisher/pub_topic")
+    pub = rospy.Publisher(pub_topic, LaserScan, queue_size=10)
+    pub_rate = rospy.get_param("/fake_scan_publisher/pub_rate")
+    rate = rospy.Rate(pub_rate) # default 20 hz
     while not rospy.is_shutdown():
         scan = LaserScan()
         scan.header.stamp = rospy.Time.now()
         scan.header.frame_id = "base_link"
-        scan.angle_min = -(2.0/3.0)*math.pi
-        scan.angle_max = (2.0/3.0)*math.pi
-        scan.angle_increment = (1/300.0)*math.pi
+        scan.angle_min = rospy.get_param("/fake_scan_publisher/angle_min") # -(2.0/3.0)*math.pi
+        scan.angle_max = rospy.get_param("/fake_scan_publisher/angle_max") # (2.0/3.0)*math.pi
+        scan.angle_increment = rospy.get_param("/fake_scan_publisher/angle_increment") # (1/300.0)*math.pi
         scan.scan_time = 0.05
-        scan.range_min = 1.0
-        scan.range_max = 10.0
+        scan.range_min = rospy.get_param("/fake_scan_publisher/range_min") # 1.0
+        scan.range_max = rospy.get_param("/fake_scan_publisher/range_max") # 10.0
         for i in range(int((scan.angle_max-scan.angle_min)/scan.angle_increment + 1)):
             scan.ranges.append(uniform(scan.range_min, scan.range_max))
         rospy.loginfo(scan)
@@ -26,6 +27,7 @@ def scanner():
 
 if __name__ == '__main__':
     try:
+        rospy.init_node('fake_scan_publisher', anonymous=True)
         scanner()
     except rospy.ROSInterruptException:
         pass
--- scripts/open_space_publisher.py
+++ scripts/open_space_publisher.py
@@ -5,7 +5,8 @@ from sensor_msgs.msg import LaserScan
 from ros_exercises.msg import OpenSpace
 
 def callback(scan):
-    pub = rospy.Publisher('open_space', OpenSpace, queue_size=10)
+    pub_topic = rospy.get_param("/open_space_publisher/pub_topic")
+    pub = rospy.Publisher(pub_topic, OpenSpace, queue_size=10)
     open_space = OpenSpace()
     open_space.distance = scan.ranges[0] 
     open_space.angle = scan.angle_min
@@ -18,9 +19,10 @@ def callback(scan):
 
 
 def listener():
-    rospy.init_node('open_space_publisher', anonymous=True)
-    rospy.Subscriber("fake_scan", LaserScan, callback)
+    sub_topic = rospy.get_param("/open_space_publisher/sub_topic")
+    rospy.Subscriber(sub_topic, LaserScan, callback)
     rospy.spin()
 
 if __name__ == '__main__':
+    rospy.init_node('open_space_publisher', anonymous=True)
     listener()
